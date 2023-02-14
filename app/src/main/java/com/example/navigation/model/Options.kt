bbe4c9062f1fef048111196e680cb0fd5f94d3b2
art <Arturlpochta@gmail.com>
2023-02-14 22:14:36 +0300

    65656

---
+++ app/src/main/java/com/example/navigation/model/Options.kt
@@ -0,0 +1,17 @@
+package com.example.navigation.model
+
+//package ua.cn.stu.navigation.activity.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Options(
+    val boxCount: Int,
+    val isTimerEnabled: Boolean
+) : Parcelable {
+
+    companion object {
+        @JvmStatic val DEFAULT = Options(boxCount = 3, isTimerEnabled = false)
+    }
+}
